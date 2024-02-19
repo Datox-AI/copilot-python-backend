bdd61128ed9aaa923b75df48a5592015be95acb9
ilkhomkhafizov <ilhomhafizov6@gmail.com>
2024-02-19 17:04:07 +0500

    added chat name generation logic

--- app/services/chats/generate_chat_name.py
+++ app/services/chats/generate_chat_name.py
@@ -6,12 +6,16 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.backend.session import create_maindb_session
-from app.models.maindb import Chat
+from app.models.maindb import Chat, Message
+from app.schemas.message import MessageMapper
+
+from app.services.messages.message_create_stream import OpenAIChatStream
 
 
 class GenerateChatName:
     def __init__(self, session: Annotated[Session, Depends(create_maindb_session)]) -> None:
         self.session = session
+        self.generator = OpenAIChatStream()
 
     def invoke(self, id: UUID) -> str:
         entity = self.session.execute(select(Chat).where(Chat.id == id, Chat.is_deleted == False))
@@ -20,6 +24,19 @@ class GenerateChatName:
         if not entity:
             raise HTTPException(status_code=404, detail="Chat not found")
 
-        # TODO: Implement name generation logic here
+        # message_objs = (
+        #     self.session.query(Message).filter(Message.chat_id == id).order_by(Message.created_at.asc())
+        # )
+        message_objs = (
+            self.session.query(Message)
+            .filter(Message.chat_id == id)
+            .order_by(Message.created_at.desc())  # Сортируем по убыванию, чтобы получить последние сообщения
+            .limit(3)  # Ограничиваем выборку тремя последними сообщениями
+            .all()  # Получаем все объекты, удовлетворяющие условиям
+        )
 
-        return "Test Name"
+        message_list = [MessageMapper.map_to_user_message_response(message_obj) for message_obj in message_objs]
+
+        generated_name = self.generator.name_generate(message_list)
+        
+        return generated_name
