1d879acecfccecbba61d7d48e21ee96168e057a3
nurmukhitdin <nurmuhammad@datox.ai>
2024-03-19 16:28:30 +0500

    added error handling

--- app/services/snowflake_integration/snowintegration.py
+++ app/services/snowflake_integration/snowintegration.py
@@ -356,51 +356,61 @@ class SnowflakeIntegrationService:
             cursor.execute(f"SHOW SCHEMAS IN DATABASE {db_name}")
             schemas = cursor.fetchall()
             return {"schemas": [schema[1] for schema in schemas]}
+        except HTTPException as e:
+        # Re-raise the exception if it's already an HTTPException
+            raise e
+        except Exception as e:
+            self.handle_common_errors(e)
         finally:
             cursor.close()
             ctx.close()
 
-    # Endpoint to select a schema and check separately for the existence of tables and views
+        # Endpoint to select a schema and check separately for the existence of tables and views
     def select_schema_logic(self, token: str, db_name: str, schema_name: str):
         snowflake_identifier_obj, selected_warehouse_obj = self._get_snowflake_identifier_obj()
 
+        ctx = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
+        cursor = ctx.cursor()
         try:
-            conn = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
-            cursor = conn.cursor()
+            cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
+            cursor.execute(f"SHOW TABLES IN {db_name}.{schema_name}")
+            tables_status = len(cursor.fetchall()) > 0
 
-            try:
-                cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
-                cursor.execute(f"SHOW TABLES IN {db_name}.{schema_name}")
-                tables_status = len(cursor.fetchall()) > 0
+            cursor.execute(f"SHOW VIEWS IN {db_name}.{schema_name}")
+            views_status = len(cursor.fetchall()) > 0
 
-                cursor.execute(f"SHOW VIEWS IN {db_name}.{schema_name}")
-                views_status = len(cursor.fetchall()) > 0
+            return {"message": f"Schema '{schema_name}' selected", "tables": tables_status, "views": views_status}
 
-                return {"message": f"Schema '{schema_name}' selected", "tables": tables_status, "views": views_status}
-
-            finally:
-                if "conn" in locals() and conn is not None:
-                    conn.close()
+        except HTTPException as e:
+            # Re-raise the exception if it's already an HTTPException
+            raise e
         except Exception as e:
             self.handle_common_errors(e)
+        finally:
+            cursor.close()
+            ctx.close()
+
 
     # Endpoint to list tables of a specific schema in a Snowflake database
     def get_tables_logic(self, token: str, db_name: str, schema_name: str):
         snowflake_identifier_obj, selected_warehouse_obj = self._get_snowflake_identifier_obj()
 
+        ctx = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
+        cursor = ctx.cursor()
         try:
-            conn = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
-            cursor = conn.cursor()
-
-            try:
-                cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
-                cursor.execute(f"SHOW TABLES IN {db_name}.{schema_name}")
-                tables = cursor.fetchall()
-                return {"tables": [table[1] for table in tables]}
-            finally:
-                cursor.close()
+            cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
+            cursor.execute(f"SHOW TABLES IN {db_name}.{schema_name}")
+            tables = cursor.fetchall()
+            return {"tables": [table[1] for table in tables]}
+        except HTTPException as e:
+            # Re-raise the exception if it's already an HTTPException
+            raise e
         except Exception as e:
             self.handle_common_errors(e)
+        finally:
+            cursor.close()
+            ctx.close()
+
 
     # Endpoint to change the user role in snowflake
     def change_default_role_logic(self, new_role_request: SnowflakeRole, token: str):
@@ -442,74 +452,89 @@ class SnowflakeIntegrationService:
             cursor.close()
             conn.close()
 
+
+
     def get_views_logic(self, token: str, db_name: str, schema_name: str):
         snowflake_identifier_obj, selected_warehouse_obj = self._get_snowflake_identifier_obj()
 
+        ctx = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
+        cursor = ctx.cursor()
         try:
-            conn = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
-            cursor = conn.cursor()
-
-            try:
-                cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
-                cursor.execute(f"SHOW VIEWS IN {db_name}.{schema_name}")
-                views = cursor.fetchall()
-                return {"views": [view[1] for view in views]}
-            finally:
-                cursor.close()
+            cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
+            cursor.execute(f"SHOW VIEWS IN {db_name}.{schema_name}")
+            views = cursor.fetchall()
+            return {"views": [view[1] for view in views]}
+        except HTTPException as e:
+            # Re-raise the exception if it's already an HTTPException
+            raise e
         except Exception as e:
-            self.handle_common_errors(e)
+            error_message = str(e).lower()
+            if 'insufficient privilege' in error_message:
+                detail_msg = "Insufficient privileges to access views."
+                raise HTTPException(status_code=403, detail=detail_msg)
+            else:
+                # General fallback for unhandled errors
+                raise HTTPException(status_code=500, detail=f"Internal Server Error: {error_message}")
+        finally:
+            cursor.close()
+            ctx.close()
+
 
     # Endpoint to list columns of a specific table or view in a Snowflake database, including name and type
 
     def get_columns_logic(self, token: str, db_name: str, schema_name: str, table_or_view_name: str):
         snowflake_identifier_obj, selected_warehouse_obj = self._get_snowflake_identifier_obj()
 
+        ctx = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
+        cursor = ctx.cursor()
         try:
-            conn = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
-            cursor = conn.cursor()
-
-            try:
-                cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
-                cursor.execute(f"DESCRIBE TABLE {db_name}.{schema_name}.{table_or_view_name}")
-                columns = cursor.fetchall()
-                # Format the columns data to include only name and type. Adjust indices based on Snowflake's response format
-                columns_data = [{"name": col[0], "type": col[1]} for col in columns]
-                return {"columns": columns_data}
-            finally:
-                cursor.close()
+            cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
+            cursor.execute(f"DESCRIBE TABLE {db_name}.{schema_name}.{table_or_view_name}")
+            columns = cursor.fetchall()
+            # Format the columns data to include only name and type. Adjust indices based on Snowflake's response format
+            columns_data = [{"name": col[0], "type": col[1]} for col in columns]
+            return {"columns": columns_data}
+        except HTTPException as e:
+            # Re-raise the exception if it's already an HTTPException
+            raise e
         except Exception as e:
             self.handle_common_errors(e)
+        finally:
+            cursor.close()
+            ctx.close()
 
     # Endpoint to preview data from a specific table or view in a Snowflake database
     def preview_data_logic(self, token: str, db_name: str, schema_name: str, table_or_view_name: str):
         snowflake_identifier_obj, selected_warehouse_obj = self._get_snowflake_identifier_obj()
 
+        ctx = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
+        cursor = ctx.cursor()
         try:
-            conn = self.create_snowflake_connection(token, snowflake_identifier_obj.account_identifier)
-            cursor = conn.cursor()
+            cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
+            query = f"SELECT * FROM {db_name}.{schema_name}.{table_or_view_name} LIMIT 10"
+            cursor.execute(query)
 
-            try:
-                cursor.execute(f"USE WAREHOUSE {selected_warehouse_obj.name}")
-                query = f"SELECT * FROM {db_name}.{schema_name}.{table_or_view_name} LIMIT 10"
-                cursor.execute(query)
+            # Fetch column names
+            column_names = [desc[0] for desc in cursor.description]
 
-                # Fetch column names
-                column_names = [desc[0] for desc in cursor.description]
+            # Fetch data and format rows
+            data_preview = []
+            for row in cursor.fetchall():
+                formatted_row = {
+                    col_name: (value if value is not None else "{none}")
+                    for col_name, value in zip(column_names, row)
+                }
+                data_preview.append(formatted_row)
 
-                # Fetch data and format rows
-                data_preview = []
-                for row in cursor.fetchall():
-                    formatted_row = {
-                        col_name: (value if value is not None else "{none}")
-                        for col_name, value in zip(column_names, row)
-                    }
-                    data_preview.append(formatted_row)
-
-                return {"data_preview": data_preview}
-            finally:
-                cursor.close()
+            return {"data_preview": data_preview}
+        except HTTPException as e:
+            # Re-raise the exception if it's already an HTTPException
+            raise e
         except Exception as e:
             self.handle_common_errors(e)
+        finally:
+            cursor.close()
+            ctx.close()
 
     # list available roles
     def get_available_roles_logic(self, token: str):
